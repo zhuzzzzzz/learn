bca6b52116f3343aa3985a93e24ae4e46cff2477
zhuzzzzzz <57058943+zhuzzzzzz@users.noreply.github.com>
2025-01-10 17:24:47 +0800

    -

--- test.py
+++ test.py
@@ -1,20 +1,20 @@
-class A:
-    def __init__(self):
-        print("A's init called")
+class UppercaseMeta(type):
+    def __new__(cls, name, bases, dct):
+        # 将所有属性名转换为大写
+        uppercase_attributes = {
+            key.upper(): value for key, value in dct.items()
+        }
+        # 使用创建的大写属性字典来创建类
+        return super().__new__(cls, name, bases, uppercase_attributes)
 
-class B(A):
-    def __init__(self):
-        super().__init__()
-        print("B's init called")
 
-class C(A):
-    def __init__(self):
-        super().__init__()
-        print("C's init called")
+class MyClass(metaclass=UppercaseMeta):
+    my_attr = "hello"
+    another_attr = "world"
 
-class D(B, C):
-    def __init__(self):
-        super().__init__()
-        print("D's init called")
 
-d = D()
+# 测试是否存在大写的属性
+print(hasattr(MyClass, 'my_attr'))  # False，因为原属性名已经变成大写
+print(hasattr(MyClass, 'MY_ATTR'))  # True，因为属性名变成了大写
+print(hasattr(MyClass, 'another_attr'))  # False
+print(hasattr(MyClass, 'ANOTHER_ATTR'))  # True
